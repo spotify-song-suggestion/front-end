ee951aaad9b7a0f5abd091bfbc03170f8c940cc7
spazdrum <spazdrum@gmail.com>
2020-08-21 00:20:51 -0400

    fixed home

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,26 +1,12 @@
 import React from "react";
 
-
-
-
-
-
-
-
 export default function Home() {
-
-
-
-
-
   return (
-    
-    <div>
-      <h1>Welcome back, {" "}</h1>
-    </div>
-    <div className="savedSongs">
-  {/* display saved cards here */}
-    </div>
+    <>
+      <div>
+        <h1>Welcome back, </h1>
+      </div>
+      <div className="savedSongs">{/* display saved cards here */}</div>
     </>
   );
 }
