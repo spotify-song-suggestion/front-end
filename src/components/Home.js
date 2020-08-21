b2654fcff783bd2a2bfd715ad0656f29662488a4
Tony Sorensen <tony48853@gmail.com>
2020-08-21 00:17:17 -0700

    Moved all files up one level.

--- src/components/Home.js
+++ src/components/Home.js
@@ -14,13 +14,13 @@ export default function Home() {
 
 
   return (
-    
+    <div>
     <div>
       <h1>Welcome back, {" "}</h1>
     </div>
     <div className="savedSongs">
   {/* display saved cards here */}
     </div>
-    </>
+    </div>
   );
 }
