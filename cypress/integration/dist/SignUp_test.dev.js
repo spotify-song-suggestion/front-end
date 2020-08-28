fae18cd4f7a8ed59b1ff8c3b54bf937136e40861
spazdrum <spazdrum@gmail.com>
2020-08-27 23:21:00 -0400

    finished signup tests

--- cypress/integration/dist/SignUp_test.dev.js
+++ cypress/integration/dist/SignUp_test.dev.js
@@ -1,28 +1,26 @@
+"use strict";
+
 describe("Test form inputs", function () {
   beforeEach(function () {
     cy.visit("http://localhost:3000/signup");
   });
-
   it("Renders Page", function () {
     cy.visit("http://localhost:3000/signup");
   });
-
   it("Buttons Disabled", function () {
     cy.get("button[type=submit]").as("submitBtn");
     cy.get("@submitBtn").should("be.disabled");
   });
-
-  it("Accepts input", () => {
-    const first = 'John';
-    const last = 'Doe';
-    const email = 'johnDoe@doejohn.com';
-    const user = 'johnDoe';
-    const pass = 'i<3Lambda';
-
+  it("Accepts input", function () {
+    var first = 'John';
+    var last = 'Doe';
+    var email = 'johnDoe@doejohn.com';
+    var user = 'johnDoe';
+    var pass = 'i<3Lambda';
     cy.get('[for="firstName"] > input').type(first).should("have.value", first);
     cy.get('[for="lastName"] > input').type(last).should("have.value", last);
     cy.get('[for="email"] > input').type(email).should("have.value", email);
     cy.get(':nth-child(4) > input').type(user).should("have.value", user);
     cy.get('[for="password"] > input').type(pass).should("have.value", pass);
-  })
+  });
 });
